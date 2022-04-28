71db1ba031fe0ddad047e42b99bcc3e815b0fc09
scheuererra68323 <scheuererra68323@th-nuernberg.de>
2022-04-28 15:39:23 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='covid-ecg',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A short description of the project.',
+    author='scheuererra68323',
+    license='MIT',
+)
