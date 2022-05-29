33dcb69fc2422376ed540e35362513a274b5d9eb
scheuererra68323 <scheuererra68323@th-nuernberg.de>
2022-05-29 13:30:35 +0200

    convert RR intervals to ms

--- covidecg/features/utils.py
+++ covidecg/features/utils.py
@@ -16,11 +16,11 @@ import biosppy.signals.ecg as ecg
 
 def get_heart_rate_features(rpeak_locs, sampling_rate):
     hr_idx, hr = biosppy.signals.tools.get_heart_rate(
-        beats=rpeaks, sampling_rate=sampling_rate, smooth=True, size=3
+        beats=rpeak_locs, sampling_rate=sampling_rate, smooth=True, size=3
     )
     return hr.mean(), hr.std()
 
-def compute_rr_intervals(rpeaks:list) -> np.ndarray:
+def compute_rr_intervals(rpeaks:list, sampling_rate:int) -> np.ndarray:
     if len(rpeaks) < 2:
         return []  # abort
     rr_intervals = []
@@ -28,6 +28,7 @@ def compute_rr_intervals(rpeaks:list) -> np.ndarray:
         right_peak_i = left_peak_i + 1
         rr_intervals.append(rpeaks[right_peak_i] - rpeaks[left_peak_i])
     rr_intervals = np.array(rr_intervals)
+    rr_intervals = rr_intervals * 1000 / sampling_rate
     return rr_intervals
 
 def compute_pr_intervals(p_onsets:list, q_onsets:list):
@@ -35,11 +36,11 @@ def compute_pr_intervals(p_onsets:list, q_onsets:list):
         raise Exception("Cannot compute PR Intervals: No P Onsets provided!")
     if len(q_onsets) < 1:
         raise Exception("Cannot compute PR Intervals: No Q Onsets provided!")
-    
+
     if len(p_onsets) != len(q_onsets):
         raise Exception(message=f"Different number of P-Onsets and Q-Onsets, cannot compute PR Intervals! - p_onsets: {len(p_onsets)}, q_onsets:{len(q_onsets)}")
         # TODO handle different lengths -> only use P-Q pairs
-    
+
     pr_intervals = []
     for _p_onset, _q_onset in zip(p_onsets, q_onsets):
         pr_intervals.append(_q_onset - _p_onset)
@@ -53,6 +54,25 @@ def get_rpeaks_locations(signal, sampling_rate):
     )
     return rpeaks_locations
 
+def get_peaks_locations(signal, sampling_rate, delineate_method='dwt', delineate_check=False):
+    print(signal.max(axis=1) - signal.min(axis=1))
+    print(signal)
+    
+    
+    peaks_locations = nk.ecg_delineate(signal, sampling_rate=sampling_rate, method=delineate_method, check=delineate_check)[1]
+
+    peaks_locations['ECG_R_Peaks'] = get_rpeaks_locations(signal, sampling_rate)
+
+    # estimate Q onset locations TODO: find method to actually determine Q onsets
+    q_onset_correction_in_samples = 10 * 1000 / sampling_rate  # shift 15ms to the left to estimate Q Onset
+    peaks_locations['ECG_Q_Onsets'] = np.array(peaks_locations['ECG_Q_Peaks']) - q_onset_correction_in_samples
+
+    # estimate S offsets
+    s_offset_correction_in_samples = 10 * 1000 / sampling_rate  # shift 15ms to the left to estimate Q Onset
+    peaks_locations['ECG_S_Offsets'] = np.array(peaks_locations['ECG_S_Peaks']) + s_offset_correction_in_samples
+
+    return peaks_locations
+
 def get_peaks_features(signal, sampling_rate):
     """Compute Peaks Features as mean+std of signal at Q, R, S, T peak locations
 
@@ -64,60 +84,58 @@ def get_peaks_features(signal, sampling_rate):
         list: Peaks features (mean+std signal amplitude at Q, R, S, T peaks)
     """
     peaks_feats = []
+    try:
+        peaks_locations = get_peaks_locations(signal, sampling_rate)
+    except:
+        print("Skipping!")
+        return np.zeros(8)
     
     # R Peaks
-    rpeaks_locations = get_rpeaks_locations(signal, sampling_rate)
-    rpeaks_y = signal[rpeaks_locations]
+    rpeaks_y = signal[peaks_locations['ECG_R_Peaks']]
     peaks_feats.append(rpeaks_y.mean())
     peaks_feats.append(rpeaks_y.std())
-    
-    other_peaks_locations = nk.ecg_delineate(signal, method='dwt', sampling_rate=sampling_rate)[1]
 
     # Q Peaks
-    # print(other_peaks_locations)
-    # print("Q Peaks:", other_peaks_locations['ECG_Q_Peaks'])
-    qpeaks_y = signal[other_peaks_locations['ECG_Q_Peaks']]
+    qpeaks_y = signal[peaks_locations['ECG_Q_Peaks']]
     peaks_feats.append(qpeaks_y.mean())
     peaks_feats.append(qpeaks_y.std())
     
     # S Peaks
-    speaks_y = signal[other_peaks_locations['ECG_S_Peaks']]
+    speaks_y = signal[peaks_locations['ECG_S_Peaks']]
     peaks_feats.append(speaks_y.mean())
     peaks_feats.append(speaks_y.std())
     
     # T Peaks
-    tpeaks_y = signal[other_peaks_locations['ECG_T_Peaks']]
+    tpeaks_y = signal[peaks_locations['ECG_T_Peaks']]
     peaks_feats.append(tpeaks_y.mean())
     peaks_feats.append(tpeaks_y.std())
+    
+    print("peaks_feats:", peaks_feats)
 
     return peaks_feats
 
 def get_interval_features(signal, sampling_rate):
     interval_feats = []
+    peaks_locations = get_peaks_locations(signal, sampling_rate)
     
     # RR Intervals
     rpeaks_locations = nk.ecg_peaks(signal, sampling_rate=sampling_rate)[1]['ECG_R_Peaks']
-    rr_intervals = compute_rr_intervals(rpeaks_locations)
+    rr_intervals = compute_rr_intervals(peaks_locations['ECG_R_Peaks'])
     interval_feats.append(rr_intervals.mean())
     interval_feats.append(rr_intervals.std())
     
-    other_peaks_locations = nk.ecg_delineate(signal, sampling_rate=sampling_rate)[1]
     
     # PR Intervals
-    # estimate Q onset locations TODO: find method to actually determine Q onsets
-    q_onset_locations = np.array(other_peaks_locations['ECG_Q_Peaks']) - 0.015
-    pr_intervals = compute_pr_intervals(other_peaks_locations['ECG_P_Onsets'], list(q_onset_locations))
+    pr_intervals = compute_pr_intervals(peaks_locations['ECG_P_Onsets'], peaks_locations['ECG_Q_Onsets'])
     interval_feats.append(pr_intervals.mean())
     interval_feats.append(pr_intervals.std())
     
-    # estimate S offsets
-    s_offset_locations = np.array(other_peaks_locations['ECG_S_Peaks']) + 0.015
-    qrs_widths = compute_pr_intervals(q_onset_locations, s_offset_locations)
+    qrs_widths = compute_pr_intervals(peaks_locations['ECG_Q_Onsets'], peaks_locations['ECG_S_Offsets'])
     interval_feats.append(qrs_widths.mean())
     interval_feats.append(qrs_widths.std())
     
     # QT intervals
-    qt_intervals = compute_pr_intervals(q_onset_locations, other_peaks_locations['ECG_T_Offsets'])
+    qt_intervals = compute_pr_intervals(peaks_locations['ECG_Q_Onsets'], peaks_locations['ECG_T_Offsets'])
     interval_feats.append(qt_intervals.mean())
     interval_feats.append(qt_intervals.std())
     
@@ -135,8 +153,13 @@ class EcgPeaksFeatsExtractor(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, x):
-        peaks_feats = [get_peaks_features(signal, sampling_rate=self.sampling_rate) for signal in x]
-        return np.array(peaks_feats)
+        peaks_feats = []
+        for lead_i in range(x.shape[1]):
+            lead_signals = x[:, lead_i]
+            lead_peaks_feats = [get_peaks_features(signal, sampling_rate=self.sampling_rate) for signal in lead_signals]
+            peaks_feats.append(lead_peaks_feats)
+        print(np.vstack(peaks_feats))
+        return np.vstack(peaks_feats)
 
 
 class EcgIntervalFeatsExtractor(BaseEstimator, TransformerMixin):
