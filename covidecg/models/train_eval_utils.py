ac129d79b575658427a9f08b47f1adaca4739aa0
scheuererra68323 <scheuererra68323@th-nuernberg.de>
2022-07-22 10:33:43 +0200

    refactor train_eval_utils

--- covidecg/models/train_eval_utils.py
+++ covidecg/models/train_eval_utils.py
@@ -20,13 +20,13 @@ import sklearn.metrics
 import sklearn.svm
 import torch.optim
 import torch
-from io import StringIO
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import SMOTE
 from collections import Counter
 import imblearn.pipeline
 import torchvision.models
 import matplotlib.pyplot as plt
+from typing import Tuple
 
 
 def build_preprocessing_pipeline(conf:dict, sampling_rate:int) -> sklearn.pipeline.Pipeline:
@@ -40,25 +40,8 @@ def build_preprocessing_pipeline(conf:dict, sampling_rate:int) -> sklearn.pipeli
         # no need to do anything - use signal value arrays as-is
         pass
     if conf['features'] == 'signal_image':
-
         # convert from array of signal values to 2D grayscale image of signal curve
         preprocessing.steps.append(('convert_signal_to_image', feature_utils.EcgSignalToImageConverter(vertical_resolution=conf['signal_image_vertical_resolution'])))
-
-        if conf['model'] in ['vgg16', 'resnet18']:
-            # convert from single channel grayscale to 3-channel RGB image representation
-            preprocessing.steps.append(('grayscale_to_rgb', FunctionTransformer(data_utils.grayscale_to_rgb)))
-            # convert to torch.Tensor for pretrained model image transforms
-            preprocessing.steps.append(('to_tensor', FunctionTransformer(torch.from_numpy)))
-
-            # apply image transforms specific to pretrained model
-            if conf['model'] == 'vgg16':
-                preprocessing.steps.append(('vgg16_image_preprocessing', FunctionTransformer(torchvision.models.VGG16_Weights.IMAGENET1K_FEATURES.transforms())))
-            elif conf['model'] == 'resnet18':
-                preprocessing.steps.append(('resnet18_image_preprocessing', FunctionTransformer(torchvision.models.ResNet18_Weights.IMAGENET1K_V1.transforms())))
-
-            # convert back to numpy array
-            preprocessing.steps.append(('to_numpy', FunctionTransformer(lambda x_tensor: x_tensor.detach().cpu().numpy())))
-    
     elif conf['features'] == 'lfcc':
         preprocessing.steps.append(('extract_lfcc', feature_utils.EcgLfccFeatsExtractor(sampling_rate=sampling_rate)))
     elif conf['features'] == 'peaks':
@@ -71,127 +54,93 @@ def build_preprocessing_pipeline(conf:dict, sampling_rate:int) -> sklearn.pipeli
                                         feature_utils.EcgPeaksFeatsExtractor(sampling_rate=sampling_rate),
                                         feature_utils.EcgIntervalsFeatsExtractor(sampling_rate=sampling_rate))))
 
+    if conf['model'] in ['vgg16', 'resnet18']:
+        # convert from single channel grayscale to 3-channel RGB image representation
+        preprocessing.steps.append(('grayscale_to_rgb', FunctionTransformer(data_utils.grayscale_to_rgb)))
+        # convert to torch.Tensor for pretrained model image transforms
+        preprocessing.steps.append(('to_tensor', FunctionTransformer(torch.from_numpy)))
+
+        # apply image transforms specific to pretrained model
+        if conf['model'] == 'vgg16':
+            preprocessing.steps.append(('vgg16_image_preprocessing', FunctionTransformer(torchvision.models.VGG16_Weights.IMAGENET1K_FEATURES.transforms())))
+        elif conf['model'] == 'resnet18':
+            preprocessing.steps.append(('resnet18_image_preprocessing', FunctionTransformer(torchvision.models.ResNet18_Weights.IMAGENET1K_V1.transforms())))
+
+        # convert back to numpy array
+        preprocessing.steps.append(('to_numpy', FunctionTransformer(lambda x_tensor: x_tensor.detach().cpu().numpy())))
+
     if conf['flatten_leads']:
         preprocessing.steps.append(('flatten_leads', FunctionTransformer(data_utils.flatten_leads)))
 
     return preprocessing
 
 
-def load_dataset(samples_list, root_dir, ecg_type):
+def load_dataset(samples_list:list, root_dir:Path, ecg_type:str) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, LabelEncoder]:
     """ Load dataset and encode targets to numerical """
-    logging.info(f"Samples list: f{samples_list}")
+    
+    logging.info(f"Samples list: {samples_list}")
     if ecg_type == 'stress':
         X_class0, y_class0, pat_ids_class0 = data_utils.load_stress_ecg_runs(samples_list[0], root_dir)
         X_class1, y_class1, pat_ids_class1 = data_utils.load_stress_ecg_runs(samples_list[1], root_dir)
     elif ecg_type == 'rest':
-        X_class0, y_class0, pat_id_class0 = data_utils.load_rest_ecg_runs(samples_list[0], root_dir)
-        X_class1, y_class1, pat_id_class0 = data_utils.load_rest_ecg_runs(samples_list[1], root_dir)
+        X_class0, y_class0, pat_ids_class0 = data_utils.load_rest_ecg_runs(samples_list[0], root_dir)
+        X_class1, y_class1, pat_ids_class0 = data_utils.load_rest_ecg_runs(samples_list[1], root_dir)
     else:
         raise Exception(f"Found invalid value '{ecg_type}' for ecg_type in experiment configuration! (stress|rest)")
 
     X = np.concatenate((X_class0, X_class1))
     y = np.concatenate((y_class0, y_class1))
-    pat_ids = np.concatenate((pat_ids_class0, pat_ids_class1))
     
     label_encoder = LabelEncoder()
     y = label_encoder.fit_transform(y).astype(np.int64)
     logging.info(f"Classes in dataset: {label_encoder.classes_}")
     
-    train_idx, test_idx = next( GroupShuffleSplit(test_size=0.2).split(X, y, groups=pat_ids) )
-    X_train, y_train = X[train_idx], y[train_idx]
-    X_test, y_test = X[test_idx], y[test_idx]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, stratify=y)
     
     logging.info(f"Class distribution in training set before imbalance mitigation: {Counter(y_train)}")
     logging.info(f"Class distribution in test set: {Counter(y_test)}")
-    
+
     return X_train, X_test, y_train, y_test, label_encoder
 
 
 def build_model(conf:dict, X_train:np.ndarray, y_train:np.ndarray) -> imblearn.pipeline.Pipeline:
-    """ Define model and optimizer according to experiment configuration """
+    """ Configure model and optimizer according to configuration files """
     
     logging.info(f"Applying {conf['imbalance_mitigation']} to mitigate class imbalance in training data...")
 
-    class_weight = None
+    # compute class weights for loss function if desired
     if conf['imbalance_mitigation'] == 'criterion_weights':
         class_weight = sklearn.utils.class_weight.compute_class_weight(
             class_weight='balanced', classes=np.unique(y_train), y=y_train)
         class_weight = torch.Tensor(class_weight)
+    else:
+        class_weight = None
 
+    # params passed to skorch.NeuralNetClassifier that are the same for all skorch-based models
+    skorch_clf_common_params = {
+        'optimizer': torch.optim.Adam,
+        'criterion': nn.CrossEntropyLoss,
+        'criterion__weight': class_weight,
+        'callbacks': [
+            EpochScoring(scoring='roc_auc', lower_is_better=False),  # additional scores to observe
+            EarlyStopping(patience=conf['early_stopping_patience'])  # Early Stopping based on validation loss
+            ],
+        'max_epochs': conf['early_stopping_max_epochs'],
+        'device': 'cuda',
+        'iterator_train__shuffle': True,  # Shuffle training data on each epoch
+    }
     
     if conf['model'] == 'svm':
-        if conf['svm_class_weight_balanced']:
-            clf = sklearn.svm.SVC(kernel=conf['svm_kernel'], class_weight='balanced')
-        else:
-            clf = sklearn.svm.SVC(kernel=conf['svm_kernel'])
-
+        clf = sklearn.svm.SVC(kernel=conf['svm_kernel'], class_weight=class_weight)
     elif conf['model'] == 'mlp':
-        clf = skorch.NeuralNetClassifier(
-            # model config
-            module=MLP,
-            module__input_size=X_train[0].size,
-            # loss config
-            criterion=nn.CrossEntropyLoss,
-            criterion__weight=class_weight,
-            optimizer=torch.optim.Adam,
-            callbacks=[
-                EpochScoring(scoring='roc_auc', lower_is_better=False),  # additional scores to observe
-                EarlyStopping(patience=conf['early_stopping_patience'])  # Early Stopping based on validation loss
-                ],
-            max_epochs=conf['early_stopping_max_epochs'], device='cuda', iterator_train__shuffle=True,  # Shuffle training data on each epoch
-        )
-
+        clf = skorch.NeuralNetClassifier(module=MLP, module__input_size=X_train[0].size, **skorch_clf_common_params)
     elif conf['model'] == 'cnn2d':
-        clf = skorch.NeuralNetClassifier(
-            # model config
-            module=CNN2D,
-            # loss config
-            criterion=nn.CrossEntropyLoss,
-            criterion__weight=class_weight,
-            optimizer=torch.optim.Adam,
-            # hyperparams
-            callbacks=[
-                EpochScoring(scoring='roc_auc', lower_is_better=False),  # additional scores to observe
-                EarlyStopping(patience=conf['early_stopping_patience'])  # Early Stopping based on validation loss
-                ],
-            max_epochs=conf['early_stopping_max_epochs'], device='cuda', iterator_train__shuffle=True  # Shuffle training data on each epoch
-        )
-
+        clf = skorch.NeuralNetClassifier(module=CNN2D, **skorch_clf_common_params)
     elif conf['model'] == 'cnn1d':
-        clf = skorch.NeuralNetClassifier(
-            # model config
-            module=CNN1D,
-            # loss config
-            criterion=nn.CrossEntropyLoss,
-            criterion__weight=class_weight,
-            optimizer=torch.optim.Adam,
-            # hyperparams
-            callbacks=[
-                EpochScoring(scoring='roc_auc', lower_is_better=False),  # additional scores to observe
-                EarlyStopping(patience=conf['early_stopping_patience'])  # Early Stopping based on validation loss
-                ],
-            max_epochs=conf['early_stopping_max_epochs'], device='cuda', iterator_train__shuffle=True  # Shuffle training data on each epoch
-        )
-        
+        clf = skorch.NeuralNetClassifier(module=CNN1D, **skorch_clf_common_params)
     elif conf['model'] == 'lstm':
-        clf = skorch.NeuralNetClassifier(
-            # model config
-            module=LSTM,
-            module__input_size=12,
-            # loss config
-            criterion=nn.CrossEntropyLoss,
-            criterion__weight=class_weight,
-            optimizer=torch.optim.Adam,
-            # hyperparams
-            callbacks=[
-                EpochScoring(scoring='roc_auc', lower_is_better=False),  # additional scores to observe
-                EarlyStopping(patience=conf['early_stopping_patience'])  # Early Stopping based on validation loss
-                ],
-            max_epochs=conf['early_stopping_max_epochs'], device='cuda', iterator_train__shuffle=True  # Shuffle training data on each epoch
-        )
-
+        clf = skorch.NeuralNetClassifier(module=LSTM, module__input_size=12, **skorch_clf_common_params)
     elif conf['model'] == 'lstmattn':
-        
         bidirectional = True
         hidden_size = 250
         n_layers_rnn = 2
@@ -203,53 +152,11 @@ def build_model(conf:dict, X_train:np.ndarray, y_train:np.ndarray) -> imblearn.p
         
         model = Classifier(encoder=rnn_encoder, attention=attention, hidden_dim=attention_dim, num_classes=2)
         
-        clf = skorch.NeuralNetClassifier(
-            # model config
-            module=model,
-            # loss config
-            criterion=nn.CrossEntropyLoss,
-            criterion__weight=class_weight,
-            optimizer=torch.optim.Adam,
-            # hyperparams
-            callbacks=[
-                EpochScoring(scoring='roc_auc', lower_is_better=False),  # additional scores to observe
-                EarlyStopping(patience=conf['early_stopping_patience'])  # Early Stopping based on validation loss
-                ],
-            max_epochs=conf['early_stopping_max_epochs'], device='cuda', iterator_train__shuffle=True  # Shuffle training data on each epoch
-        )
-    
+        clf = skorch.NeuralNetClassifier(module=model, **skorch_clf_common_params)
     elif conf['model'] == 'vgg16':
-       clf = skorch.NeuralNetClassifier(
-            # model config
-            module=VGG16Classifier,
-            # loss config
-            criterion=nn.CrossEntropyLoss,
-            criterion__weight=class_weight,
-            optimizer=torch.optim.Adam,
-            # hyperparams
-            callbacks=[
-                EpochScoring(scoring='roc_auc', lower_is_better=False),  # additional scores to observe
-                EarlyStopping(patience=conf['early_stopping_patience'])  # Early Stopping based on validation loss
-                ],
-            max_epochs=conf['early_stopping_max_epochs'], device='cuda', iterator_train__shuffle=True  # Shuffle training data on each epoch
-        )
-     
+       clf = skorch.NeuralNetClassifier(module=VGG16Classifier, **skorch_clf_common_params)
     elif conf['model'] == 'resnet18':
-        clf = skorch.NeuralNetClassifier(
-            # model config
-            module=ResNet18Classifier,
-            # loss config
-            criterion=nn.CrossEntropyLoss,
-            criterion__weight=class_weight,
-            optimizer=torch.optim.Adam,
-            # hyperparams
-            callbacks=[
-                EpochScoring(scoring='roc_auc', lower_is_better=False),  # additional scores to observe
-                EarlyStopping(patience=conf['early_stopping_patience'])  # Early Stopping based on validation loss
-                ],
-            max_epochs=conf['early_stopping_max_epochs'], device='cuda', iterator_train__shuffle=True  # Shuffle training data on each epoch
-        )
-         
+        clf = skorch.NeuralNetClassifier(module=ResNet18Classifier, **skorch_clf_common_params)
 
     # add under/oversampling steps to model pipeline if desired
     if conf['imbalance_mitigation'] == 'smote':
@@ -264,27 +171,20 @@ def build_model(conf:dict, X_train:np.ndarray, y_train:np.ndarray) -> imblearn.p
     return pipe
 
 
-def evaluate_experiment(X_test, y_true, y_encoder, gs):
+def evaluate_experiment(X_test:np.ndarray, y_true:np.ndarray, y_encoder:LabelEncoder, gs:imblearn.pipeline.Pipeline) -> None:
     """ Compute scores, create figures and log all metrics to MLFlow """
+
+    # Generate Confusion Matrix
+    conf_matrix_fig = sklearn.metrics.ConfusionMatrixDisplay.from_estimator(gs, X_test, y_true, display_labels=y_encoder.classes_, cmap='Blues', normalize='true').figure_
+    mlflow.log_figure(conf_matrix_fig, 'confusion_matrix.png')
     
-    y_pred = gs.predict(X_test)
+    # Generate ROC curve
+    roc_curve_fig = sklearn.metrics.RocCurveDisplay.from_estimator(gs, X_test, y_true).figure_
+    mlflow.log_figure(roc_curve_fig, 'roc_curve.png')
     
-    accuracy_score = sklearn.metrics.accuracy_score(y_true, y_pred)
-    roc_auc_score = sklearn.metrics.roc_auc_score(y_true, y_pred)
-    f1_score = sklearn.metrics.f1_score(y_true, y_pred)
-    precision_score = sklearn.metrics.precision_score(y_true, y_pred)
-    recall_score = sklearn.metrics.recall_score(y_true, y_pred)
-    
-    conf_matrix_figure = sklearn.metrics.ConfusionMatrixDisplay.from_predictions(y_true, y_pred, display_labels=y_encoder.classes_, cmap='Blues', normalize='true').figure_
-    
-    roc_auc_curve_figure = sklearn.metrics.RocCurveDisplay.from_estimator(gs, X_test, y_true).figure_
-    
-    logging.info("Logging metrics to MLFlow...")
-    mlflow.log_figure(conf_matrix_figure, 'confusion_matrix.png')
-    mlflow.log_figure(roc_auc_curve_figure, 'roc_auc_curve.png')
-    
+    # Generate train&valid Loss curve
     loss_fig = plt.figure()
-    plt.plot(gs.best_estimator_.steps[-1][1].history[:, 'train_loss'], label='train_loss')
-    plt.plot(gs.best_estimator_.steps[-1][1].history[:, 'valid_loss'], label='valid_loss')
+    plt.plot(gs.best_estimator_.steps[-1][1].history[:, 'train_loss'], label='train loss')
+    plt.plot(gs.best_estimator_.steps[-1][1].history[:, 'valid_loss'], label='valid loss')
     plt.legend()
-    mlflow.log_figure(loss_fig, 'loss.png')
+    mlflow.log_figure(loss_fig, 'train_valid_loss.png')
