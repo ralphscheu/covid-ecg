171fe40d3d857974bd57e304e83942c5dab722a5
scheuererra68323 <scheuererra68323@th-nuernberg.de>
2022-09-15 11:08:48 +0200

    rename dataset mmc_recs to mmc

--- 1-load-data.sh
+++ 1-load-data.sh
@@ -9,18 +9,18 @@
 # #####################
 
 # Extract all recordings for postcovid patient group
-# rm -rf data/interim/mmc_recs_csv/postcovid
+# rm -rf data/interim/mmc_csv/postcovid
 # python3 covidecg/data/extract_recordings.py \
 #     --prefix postcovid \
 #     --patients-list data/raw/patients_postcovid.csv \
-#     data/raw/ecg_export_postcovid data/interim/mmc_recs_csv/postcovid
+#     data/raw/ecg_export_postcovid data/interim/mmc_csv/postcovid
 
 # # Extract all recordings for control group
-# rm -rf data/interim/mmc_recs_csv/ctrl
+# rm -rf data/interim/mmc_csv/ctrl
 # python3 covidecg/data/extract_recordings.py \
 #     --prefix ctrl \
 #     --patients-list data/raw/patients_ctrl.csv \
-#     data/raw/ecg_export_ctrl data/interim/mmc_recs_csv/ctrl
+#     data/raw/ecg_export_ctrl data/interim/mmc_csv/ctrl
 
 
 
@@ -30,21 +30,21 @@
 # ### GENERATE ECG IMAGES
 # #####################
 
-# rm -rf data/interim/mmc_recs/postcovid
-# python3 covidecg/data/create_images.py --img-height 100 \
-#     --recordings-file data/interim/mmc_recs_stress_postcovid.csv \
-#     --recordings-dir data/interim/mmc_recs_csv/postcovid \
-#     --output-dir data/interim/mmc_recs/postcovid
+rm -rf data/interim/mmc/postcovid
+python3 covidecg/data/create_images.py --img-height 100 \
+    --recordings-file data/interim/mmc_stress_postcovid.csv \
+    --recordings-dir data/interim/mmc_csv/postcovid \
+    --output-dir data/interim/mmc/postcovid
 
-# rm -rf data/interim/mmc_recs/ctrl
-# python3 covidecg/data/create_images.py --img-height 100 \
-#     --recordings-file data/interim/mmc_recs_stress_ctrl.csv \
-#     --recordings-dir data/interim/mmc_recs_csv/ctrl \
-#     --output-dir data/interim/mmc_recs/ctrl
+rm -rf data/interim/mmc/ctrl
+python3 covidecg/data/create_images.py --img-height 100 \
+    --recordings-file data/interim/mmc_stress_ctrl.csv \
+    --recordings-dir data/interim/mmc_csv/ctrl \
+    --output-dir data/interim/mmc/ctrl
 
-# rm -rf data/interim/mmc_recs_10s
-# python covidecg/data/filter_and_symlink.py --min-length 5000 --max-length 5000 data/interim/mmc_recs/ctrl data/interim/mmc_recs_10s/ctrl
-# python covidecg/data/filter_and_symlink.py --min-length 5000 --max-length 5000 data/interim/mmc_recs/postcovid data/interim/mmc_recs_10s/postcovid
+rm -rf data/interim/mmc_10s
+python covidecg/data/filter_and_symlink.py --min-length 5000 --max-length 5000 data/interim/mmc/ctrl data/interim/mmc_10s/ctrl
+python covidecg/data/filter_and_symlink.py --min-length 5000 --max-length 5000 data/interim/mmc/postcovid data/interim/mmc_10s/postcovid
 
 
 
@@ -56,11 +56,11 @@
 
 # # python3 covidecg/data/concat_recordings_per_session.py \
 # #     --recordings-list data/interim/recordings_stress_ecg_postcovid.csv \
-# #     data/interim/mmc_recs/postcovid data/interim/mmc_sessions_postcovid
+# #     data/interim/mmc/postcovid data/interim/mmc_sessions_postcovid
 
 # # python3 covidecg/data/concat_recordings_per_session.py \
 # #     --recordings-list data/interim/recordings_stress_ecg_ctrl.csv \
-# #     data/interim/mmc_recs/ctrl data/interim/mmc_sessions_ctrl
+# #     data/interim/mmc/ctrl data/interim/mmc_sessions_ctrl
 
 
 
@@ -79,38 +79,38 @@
 #         "${file}"
 # done
 
-rm -rf data/interim/khan2021/covid
-for file in ./data/external/ECG\ Images\ of\ COVID-19\ Patients\ \(250\)/Binder*.jpg; do
-    echo "Processing ${file}"
-	python3 covidecg/data/load_khan2021_dataset.py --img-height 100 \
-		--output-dir data/interim/khan2021/covid \
-		--input-layout binder \
-        "${file}"
-done
-for file in ./data/external/ECG\ Images\ of\ COVID-19\ Patients\ \(250\)/COVID*.jpg; do
-    echo "Processing ${file}"
-	python3 covidecg/data/load_khan2021_dataset.py --img-height 100 \
-		--output-dir data/interim/khan2021/covid \
-		--input-layout ecgsheet \
-        "${file}"
-done
+# rm -rf data/interim/khan2021/covid
+# for file in ./data/external/ECG\ Images\ of\ COVID-19\ Patients\ \(250\)/Binder*.jpg; do
+#     echo "Processing ${file}"
+# 	python3 covidecg/data/load_khan2021_dataset.py --img-height 100 \
+# 		--output-dir data/interim/khan2021/covid \
+# 		--input-layout binder \
+#         "${file}"
+# done
+# for file in ./data/external/ECG\ Images\ of\ COVID-19\ Patients\ \(250\)/COVID*.jpg; do
+#     echo "Processing ${file}"
+# 	python3 covidecg/data/load_khan2021_dataset.py --img-height 100 \
+# 		--output-dir data/interim/khan2021/covid \
+# 		--input-layout ecgsheet \
+#         "${file}"
+# done
 
 
 
 
 
-# Generate train/test sets for mmc_recs_postcovid_vs_ctrl
-rm -rf data/processed/mmc_recs_postcovid_vs_ctrl
-splitfolders --output data/processed/mmc_recs_postcovid_vs_ctrl --ratio .8 .2 -- data/interim/mmc_recs
+# Generate train/test sets for mmc_postcovid_vs_ctrl
+# rm -rf data/processed/mmc_postcovid_vs_ctrl
+# splitfolders --output data/processed/mmc_postcovid_vs_ctrl --ratio .8 .2 -- data/interim/mmc
 
-# Generate train/test sets for mmc_recs_10s_postcovid_vs_ctrl
-rm -rf data/processed/mmc_recs_10s_postcovid_vs_ctrl
-splitfolders --output data/processed/mmc_recs_10s_postcovid_vs_ctrl --ratio .8 .2 -- data/interim/mmc_recs_10s
-
-
+# # Generate train/test sets for mmc_10s_postcovid_vs_ctrl
+# rm -rf data/processed/mmc_10s_postcovid_vs_ctrl
+# splitfolders --output data/processed/mmc_10s_postcovid_vs_ctrl --ratio .8 .2 -- data/interim/mmc_10s
 
 
-# Generate train/test sets for khan2021_covid_vs_normal
-rm -rf data/processed/khan2021_covid_vs_normal
-splitfolders --output data/processed/khan2021_covid_vs_normal --ratio .8 .2 -- data/interim/khan2021
+
+
+# # Generate train/test sets for khan2021_covid_vs_normal
+# rm -rf data/processed/khan2021_covid_vs_normal
+# splitfolders --output data/processed/khan2021_covid_vs_normal --ratio .8 .2 -- data/interim/khan2021
 
