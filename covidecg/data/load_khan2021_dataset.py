7d454a53fd2e7d6892b29bffcc2fa018eae6f213
scheuererra68323 <scheuererra68323@th-nuernberg.de>
2022-11-03 10:05:56 +0100

    improved khan2021 image processing

--- covidecg/data/load_khan2021_dataset.py
+++ covidecg/data/load_khan2021_dataset.py
@@ -10,85 +10,43 @@ import os
 from pathlib import Path
 from PIL import Image
 
-# Define constants for extracting leads from scanned ECG sheet
-ECGSHEET_LEAD_WIDTH = 430
-ECGSHEET_COL0_RIGHT = 642
-ECGSHEET_COL1_RIGHT = 1134
-ECGSHEET_COL2_RIGHT = 1625
-ECGSHEET_COL3_RIGHT = 2114
-ECGSHEET_COL0_LEFT = ECGSHEET_COL0_RIGHT - ECGSHEET_LEAD_WIDTH
-ECGSHEET_COL1_LEFT = ECGSHEET_COL1_RIGHT - ECGSHEET_LEAD_WIDTH
-ECGSHEET_COL2_LEFT = ECGSHEET_COL2_RIGHT - ECGSHEET_LEAD_WIDTH
-ECGSHEET_COL3_LEFT = ECGSHEET_COL3_RIGHT - ECGSHEET_LEAD_WIDTH
-ECGSHEET_LEAD_HEIGHT = 240
-ECGSHEET_ROW0_BASELINE = 461
-ECGSHEET_ROW1_BASELINE = 762
-ECGSHEET_ROW2_BASELINE = 1065
-ECGSHEET_ROW0_TOP = ECGSHEET_ROW0_BASELINE - (ECGSHEET_LEAD_HEIGHT // 2)
-ECGSHEET_ROW0_BOTTOM = ECGSHEET_ROW0_BASELINE + (ECGSHEET_LEAD_HEIGHT // 2)
-ECGSHEET_ROW1_TOP = ECGSHEET_ROW1_BASELINE - (ECGSHEET_LEAD_HEIGHT // 2)
-ECGSHEET_ROW1_BOTTOM = ECGSHEET_ROW1_BASELINE + (ECGSHEET_LEAD_HEIGHT // 2)
-ECGSHEET_ROW2_TOP = ECGSHEET_ROW2_BASELINE - (ECGSHEET_LEAD_HEIGHT // 2)
-ECGSHEET_ROW2_BOTTOM = ECGSHEET_ROW2_BASELINE + (ECGSHEET_LEAD_HEIGHT // 2)
 
-def filter_fn(x, cutoff=50):
-    # set pixel to white if brighter than cutoff value
-    return 255 if x > cutoff else x
-filter_fn = np.vectorize(filter_fn)
-
-def remove_background_grid(img):
-    """ Remove background millimeter grid in Khan2021 scans """
-    img = cv2.fastNlMeansDenoising(np.uint8(img), dst=None, h=20, templateWindowSize=4, searchWindowSize=20)
-    img = filter_fn(img).astype('float32')
-    return img
-
-def do_enhance(im, decision_value=220):
-    im_contrast = im.copy()
-    im_contrast[im_contrast < decision_value] = 0
-    im_contrast[im_contrast >= decision_value] = 255
-    return im_contrast
-
-def ecgsheet_to_ecgimgdata(im, img_height=100):
-    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY )  # convert to grayscale
-    lead_I = im[ECGSHEET_ROW0_TOP:ECGSHEET_ROW0_BOTTOM, ECGSHEET_COL0_LEFT:ECGSHEET_COL0_RIGHT]
-    lead_II = im[ECGSHEET_ROW1_TOP:ECGSHEET_ROW1_BOTTOM, ECGSHEET_COL0_LEFT:ECGSHEET_COL0_RIGHT]
-    lead_III = im[ECGSHEET_ROW2_TOP:ECGSHEET_ROW2_BOTTOM, ECGSHEET_COL0_LEFT:ECGSHEET_COL0_RIGHT]
-    lead_aVR = im[ECGSHEET_ROW0_TOP:ECGSHEET_ROW0_BOTTOM, ECGSHEET_COL1_LEFT:ECGSHEET_COL1_RIGHT]
-    lead_aVL = im[ECGSHEET_ROW1_TOP:ECGSHEET_ROW1_BOTTOM, ECGSHEET_COL1_LEFT:ECGSHEET_COL1_RIGHT]
-    lead_aVF = im[ECGSHEET_ROW2_TOP:ECGSHEET_ROW2_BOTTOM, ECGSHEET_COL1_LEFT:ECGSHEET_COL1_RIGHT]
-    lead_V1 = im[ECGSHEET_ROW0_TOP:ECGSHEET_ROW0_BOTTOM, ECGSHEET_COL2_LEFT:ECGSHEET_COL2_RIGHT]
-    lead_V2 = im[ECGSHEET_ROW1_TOP:ECGSHEET_ROW1_BOTTOM, ECGSHEET_COL2_LEFT:ECGSHEET_COL2_RIGHT]
-    lead_V3 = im[ECGSHEET_ROW2_TOP:ECGSHEET_ROW2_BOTTOM, ECGSHEET_COL2_LEFT:ECGSHEET_COL2_RIGHT]
-    lead_V4 = im[ECGSHEET_ROW0_TOP:ECGSHEET_ROW0_BOTTOM, ECGSHEET_COL3_LEFT:ECGSHEET_COL3_RIGHT]
-    lead_V5 = im[ECGSHEET_ROW1_TOP:ECGSHEET_ROW1_BOTTOM, ECGSHEET_COL3_LEFT:ECGSHEET_COL3_RIGHT]
-    lead_V6 = im[ECGSHEET_ROW2_TOP:ECGSHEET_ROW2_BOTTOM, ECGSHEET_COL3_LEFT:ECGSHEET_COL3_RIGHT]
-    
-    arr = []
-    for l in [lead_I, lead_II, lead_III, lead_aVR, lead_aVL, lead_aVF, lead_V1, lead_V2, lead_V3, lead_V4, lead_V5, lead_V6]:
-        l_filtered = remove_background_grid(l)
-        # l_filtered.shape[1] // 2 -> reduce width by 50% so 100px equals 1 second as in other data sources
-        l_filtered = cv2.resize(l_filtered, (l_filtered.shape[1] // 2, img_height), interpolation=cv2.INTER_AREA)
-        arr.append(l_filtered)
-    arr = np.stack(arr, axis=0)
-    return arr
-
-
-def binder_to_ecgimgdata(img, img_height, enhance, decision_value):
-    img_color = img.copy()
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY )  # convert to grayscale
-    # Crop on grayscale version to remove whitespace
-    nonzero_coords = cv2.findNonZero(~img) # Find all non-zero points
+def crop_to_grid(im):
+    # Gen lower mask (0-5) and upper mask (175-180) of RED
+    im_hsv = cv2.cvtColor(im, cv2.COLOR_RGB2HSV)
+    mask1 = cv2.inRange(im_hsv, (  0, 50, 30), (10,   255, 255))
+    mask2 = cv2.inRange(im_hsv, (170, 50, 30), (180, 255, 255))
+    # Merge the mask and crop the red regions
+    mask = cv2.bitwise_or(mask1, mask2 )
+    nonzero_coords = cv2.findNonZero(mask)
     x, y, w, h = cv2.boundingRect(nonzero_coords) # Find minimum spanning bounding box
-    
-    img_color = img_color[y:y+h, x:x+w]
-    img_color[img_color[:, :, 0] > 170] = 255
-    img = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY )  # convert to grayscale
-    img = cv2.resize(img, (730, 430), interpolation=cv2.INTER_AREA)  # resize to common dimensions
+    return im[y:y+h, x:x+w]
 
-    lead_width, lead_height = 145, 84
-    row0_baseline, row1_baseline, row2_baseline = 60, 168, 277
-    col0_left, col1_left, col2_left, col3_left = 34, 223, 391, 566
-    
+
+def remove_grid_background(im, cutoff_value):
+    im[im >= cutoff_value] = 255
+    return im
+
+
+def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=3.0, threshold=0):
+    """Return a sharpened version of the image, using an unsharp mask."""
+    blurred = cv2.GaussianBlur(image, kernel_size, sigma)
+    sharpened = float(amount + 1) * image - float(amount) * blurred
+    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+    sharpened = sharpened.round().astype(np.uint8)
+    if threshold > 0:
+        low_contrast_mask = np.absolute(image - blurred) < threshold
+        np.copyto(sharpened, image, where=low_contrast_mask)
+    return sharpened
+
+
+def slice_ecgsheet_scan(img, img_height):
+
+    lead_width, lead_height = 430, 240
+    row0_baseline, row1_baseline, row2_baseline = 181, 480, 783
+    col0_left, col1_left, col2_left, col3_left = 134, 635, 1120, 1615
+
     lead_I =    img[row0_baseline - lead_height // 2:row0_baseline + lead_height // 2, col0_left:col0_left+lead_width]
     lead_II =   img[row1_baseline - lead_height // 2:row1_baseline + lead_height // 2, col0_left:col0_left+lead_width]
     lead_III =  img[row2_baseline - lead_height // 2:row2_baseline + lead_height // 2, col0_left:col0_left+lead_width]
@@ -104,10 +62,47 @@ def binder_to_ecgimgdata(img, img_height, enhance, decision_value):
     
     arr = []
     for lead in [lead_I, lead_II, lead_III, lead_aVR, lead_aVL, lead_aVF, lead_V1, lead_V2, lead_V3, lead_V4, lead_V5, lead_V6]:
-        # lead.shape[1] * (100/70) -> expand width by (100/70) so 100px equals 1 second as in other data sources
-        lead = cv2.resize(lead, (int(lead.shape[1] * (100.0/70.0)), img_height), interpolation=cv2.INTER_AREA)
-        if enhance:
-            lead = do_enhance(lead, decision_value)
+        # lead.shape[1] // 2 -> reduce width by 50% so 100px equals 1 second as in other data sources
+        lead = cv2.resize(lead, (lead.shape[1] // 2, img_height), interpolation=cv2.INTER_AREA)
+        arr.append(lead)
+    arr = np.stack(arr, axis=0)
+    return arr
+
+
+def slice_binder_scan(img, img_height, binder_layout):
+    print(f"img.shape: {img.shape}")
+    if binder_layout == 'portrait':
+        lead_width, lead_height = 145, 84
+        row0_baseline, row1_baseline, row2_baseline = 42, 152, 264
+        col0_left, col1_left, col2_left, col3_left = 48, 221, 396, 566
+        img = cv2.resize(img, (723, 432), cv2.INTER_CUBIC)
+    elif binder_layout == 'landscape':
+        lead_width, lead_height = 245, 143
+        row0_baseline, row1_baseline, row2_baseline = 73, 262, 452
+        col0_left, col1_left, col2_left, col3_left = 81, 375, 670, 965
+        img = cv2.resize(img, (1232, 735), cv2.INTER_CUBIC)
+    else:
+        raise Exception(f"Invalid value {binder_layout} for binder_layout")
+    
+    lead_I =    img[row0_baseline - lead_height // 2:row0_baseline + lead_height // 2, col0_left:col0_left+lead_width]
+    lead_II =   img[row1_baseline - lead_height // 2:row1_baseline + lead_height // 2, col0_left:col0_left+lead_width]
+    lead_III =  img[row2_baseline - lead_height // 2:row2_baseline + lead_height // 2, col0_left:col0_left+lead_width]
+    lead_aVR =  img[row0_baseline - lead_height // 2:row0_baseline + lead_height // 2, col1_left:col1_left+lead_width]
+    lead_aVL =  img[row1_baseline - lead_height // 2:row1_baseline + lead_height // 2, col1_left:col1_left+lead_width]
+    lead_aVF =  img[row2_baseline - lead_height // 2:row2_baseline + lead_height // 2, col1_left:col1_left+lead_width]
+    lead_V1 =   img[row0_baseline - lead_height // 2:row0_baseline + lead_height // 2, col2_left:col2_left+lead_width]
+    lead_V2 =   img[row1_baseline - lead_height // 2:row1_baseline + lead_height // 2, col2_left:col2_left+lead_width]
+    lead_V3 =   img[row2_baseline - lead_height // 2:row2_baseline + lead_height // 2, col2_left:col2_left+lead_width]
+    lead_V4 =   img[row0_baseline - lead_height // 2:row0_baseline + lead_height // 2, col3_left:col3_left+lead_width]
+    lead_V5 =   img[row1_baseline - lead_height // 2:row1_baseline + lead_height // 2, col3_left:col3_left+lead_width]
+    lead_V6 =   img[row2_baseline - lead_height // 2:row2_baseline + lead_height // 2, col3_left:col3_left+lead_width]
+    print(f"col3_left+lead_width: {col3_left+lead_width}")
+    
+    arr = []
+    for lead in [lead_I, lead_II, lead_III, lead_aVR, lead_aVL, lead_aVF, lead_V1, lead_V2, lead_V3, lead_V4, lead_V5, lead_V6]:
+        print(f"lead.shape: {lead.shape}")
+        # lead_width * (100/70) -> expand width by (100/70) so 100px equals 1 second as in other data sources
+        lead = cv2.resize(lead, (int(lead_width * (100.0/70.0)), img_height), interpolation=cv2.INTER_AREA)
         arr.append(lead)
     arr = np.stack(arr, axis=0)
     return arr
@@ -118,22 +113,42 @@ def binder_to_ecgimgdata(img, img_height, enhance, decision_value):
 @click.option('--output-dir', required=True, type=click.Path(path_type=Path, file_okay=False))
 @click.option('--input-layout', required=True, type=click.Choice(['ecgsheet', 'binder']))
 @click.option('--img-height', default=100, type=int)
-@click.option('--enhance', is_flag=True, default=False)
-@click.option('--enhance-decision-value', default=220, type=int)
-def main(in_file, output_dir, input_layout, img_height, enhance, enhance_decision_value):
+def main(in_file, output_dir, input_layout, img_height):
     
     os.makedirs(output_dir, exist_ok=True)
 
-    input_img = cv2.imread(str(in_file))  # load ECG image
+    im = cv2.imread(str(in_file))  # load ECG image
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)  # convert to RGB color space
+    
+    if input_layout == 'binder':
+        binder_layout = 'portrait' if im.shape[0] > im.shape[1] else 'landscape'
+    else:
+        binder_layout = None
+    
+    im = crop_to_grid(im)  # crop out irrelevant parts
+
+    RMGRID_CUTOFF_ECGSHEET = 40
+    RMGRID_CUTOFF_BINDER = 170
 
     if input_layout == 'ecgsheet':
-        ecg_img_data = ecgsheet_to_ecgimgdata(input_img, img_height)  # 3D numpy array containing extracted images for 12 ECG leads
+        im = remove_grid_background(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY), RMGRID_CUTOFF_ECGSHEET)  # remove grid in background
+        
+        im = unsharp_mask(im)
+        
+        ecg_grid_array = slice_ecgsheet_scan(im, img_height)
+        
     elif input_layout == 'binder':
-        ecg_img_data = binder_to_ecgimgdata(input_img, img_height, enhance, enhance_decision_value)
+        im = remove_grid_background(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY), RMGRID_CUTOFF_BINDER)  # remove grid in background
+        
+        im = unsharp_mask(im)
+        
+        ecg_grid_array = slice_binder_scan(im, img_height, binder_layout)
 
+    # arrange ECG leads in grid and save as png
     ecg_leads_grid_savepath = os.path.join(output_dir, in_file.stem + '.png')
-    ecg_leads_grid = data_utils.generate_ecg_leads_grid(ecg_img_data)
+    ecg_leads_grid = data_utils.generate_ecg_leads_grid(ecg_grid_array)
     Image.fromarray(ecg_leads_grid).convert('L').save(ecg_leads_grid_savepath)
+    print(f"Processed {in_file}. Saved output in {ecg_leads_grid_savepath}")
 
 
 if __name__ == '__main__':
--- scripts/generate-khan2021-tasks.sh
+++ scripts/generate-khan2021-tasks.sh
@@ -19,36 +19,21 @@ ln -s `pwd`/data/interim/_khan2021_covid/*.png `pwd`/data/interim/khan2021_covid
 ln -s `pwd`/data/interim/_khan2021_normal/*.png `pwd`/data/interim/khan2021_covid_khan2021_normal/0_normal/
 
 
-# khan2021 covid (enhanced) vs. normal
-rm -rf data/interim/khan2021_covidenhanced_khan2021_normal
-mkdir -p data/interim/khan2021_covidenhanced_khan2021_normal/1_covid
-mkdir -p data/interim/khan2021_covidenhanced_khan2021_normal/0_normal
-ln -s `pwd`/data/interim/_khan2021_covidenhanced/*.png `pwd`/data/interim/khan2021_covidenhanced_khan2021_normal/1_covid/
-ln -s `pwd`/data/interim/_khan2021_normal/*.png `pwd`/data/interim/khan2021_covidenhanced_khan2021_normal/0_normal/
+
+# khan2021 abnormal vs. normal (undersampled to 397 samples)
+rm -rf data/interim/khan2021_abnormal_khan2021_normal397
+mkdir -p data/interim/khan2021_abnormal_khan2021_normal397/1_abnormal
+mkdir -p data/interim/khan2021_abnormal_khan2021_normal397/0_normal
+ln -s `pwd`/data/interim/_khan2021_abnormal/*.png `pwd`/data/interim/khan2021_abnormal_khan2021_normal397/1_abnormal/
+ln -s `pwd`/data/interim/_khan2021_normal397/*.png `pwd`/data/interim/khan2021_abnormal_khan2021_normal397/0_normal/
 
 
-# khan2021 abnormal vs. normal (undersampled to 548 samples)
-rm -rf data/interim/khan2021_abnormal_khan2021_normal548
-mkdir -p data/interim/khan2021_abnormal_khan2021_normal548/1_abnormal
-mkdir -p data/interim/khan2021_abnormal_khan2021_normal548/0_normal
-ln -s `pwd`/data/interim/_khan2021_abnormal/*.png `pwd`/data/interim/khan2021_abnormal_khan2021_normal548/1_abnormal/
-ln -s `pwd`/data/interim/_khan2021_normal548/*.png `pwd`/data/interim/khan2021_abnormal_khan2021_normal548/0_normal/
-
-
-# khan2021 covid vs. normal (undersampled to 250 samples chosen from the 548 from above)
+# khan2021 covid vs. normal (undersampled to 250 samples chosen from the 397 from above)
 rm -rf data/interim/khan2021_covid_khan2021_normal250
 mkdir -p data/interim/khan2021_covid_khan2021_normal250/1_covid
 mkdir -p data/interim/khan2021_covid_khan2021_normal250/0_normal
 ln -s `pwd`/data/interim/_khan2021_covid/*.png `pwd`/data/interim/khan2021_covid_khan2021_normal250/1_covid/
 ln -s `pwd`/data/interim/_khan2021_normal250/*.png `pwd`/data/interim/khan2021_covid_khan2021_normal250/0_normal/
-
-
-# khan2021 covid (enhanced) vs. normal (undersampled to 250 samples chosen from the 548 from above)
-rm -rf data/interim/khan2021_covidenhanced_khan2021_normal250
-mkdir -p data/interim/khan2021_covidenhanced_khan2021_normal250/1_covid
-mkdir -p data/interim/khan2021_covidenhanced_khan2021_normal250/0_normal
-ln -s `pwd`/data/interim/_khan2021_covidenhanced/*.png `pwd`/data/interim/khan2021_covidenhanced_khan2021_normal250/1_covid/
-ln -s `pwd`/data/interim/_khan2021_normal250/*.png `pwd`/data/interim/khan2021_covidenhanced_khan2021_normal250/0_normal/
 
 
 
@@ -73,25 +58,14 @@ splitfolders --output data/processed/khan2021_covid_khan2021_normal --ratio .8 .
 mv data/processed/khan2021_covid_khan2021_normal/val data/processed/khan2021_covid_khan2021_normal/test
 
 
-# Generate train/test sets for khan2021_covidenhanced_khan2021_normal
-rm -rf data/processed/khan2021_covidenhanced_khan2021_normal
-splitfolders --output data/processed/khan2021_covidenhanced_khan2021_normal --ratio .8 .2 -- data/interim/khan2021_covidenhanced_khan2021_normal
-mv data/processed/khan2021_covidenhanced_khan2021_normal/val data/processed/khan2021_covidenhanced_khan2021_normal/test
 
-
-# Generate train/test sets for khan2021_abnormal_khan2021_normal548
-rm -rf data/processed/khan2021_abnormal_khan2021_normal548
-splitfolders --output data/processed/khan2021_abnormal_khan2021_normal548 --ratio .8 .2 -- data/interim/khan2021_abnormal_khan2021_normal548
-mv data/processed/khan2021_abnormal_khan2021_normal548/val data/processed/khan2021_abnormal_khan2021_normal548/test
+# Generate train/test sets for khan2021_abnormal_khan2021_normal397
+rm -rf data/processed/khan2021_abnormal_khan2021_normal397
+splitfolders --output data/processed/khan2021_abnormal_khan2021_normal397 --ratio .8 .2 -- data/interim/khan2021_abnormal_khan2021_normal397
+mv data/processed/khan2021_abnormal_khan2021_normal397/val data/processed/khan2021_abnormal_khan2021_normal397/test
 
 
 # Generate train/test sets for khan2021_covid_khan2021_normal250
 rm -rf data/processed/khan2021_covid_khan2021_normal250
 splitfolders --output data/processed/khan2021_covid_khan2021_normal250 --ratio .8 .2 -- data/interim/khan2021_covid_khan2021_normal250
 mv data/processed/khan2021_covid_khan2021_normal250/val data/processed/khan2021_covid_khan2021_normal250/test
-
-
-# Generate train/test sets for khan2021_covidenhanced_khan2021_normal250
-rm -rf data/processed/khan2021_covidenhanced_khan2021_normal250
-splitfolders --output data/processed/khan2021_covidenhanced_khan2021_normal250 --ratio .8 .2 -- data/interim/khan2021_covidenhanced_khan2021_normal250
-mv data/processed/khan2021_covidenhanced_khan2021_normal250/val data/processed/khan2021_covidenhanced_khan2021_normal250/test
