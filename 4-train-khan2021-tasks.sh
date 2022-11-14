503d9f7ea378ac524206e2f6e730202b16294566
scheuererra68323 <scheuererra68323@th-nuernberg.de>
2022-11-14 13:35:59 +0100

    CNN2D Varianten in Training Skript einbeziehen

--- 4-train-khan2021-tasks.sh
+++ 4-train-khan2021-tasks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for model in CNN3DSeqReducedMeanStdPool CNN3DSeqReducedAttnPool CNN3DSeqMeanStdPool CNN3DSeqAttnPool CNN3DSeqReducedLSTM CNN3DSeqLSTM; do
+for model in CNN3DSeqReducedMeanStdPool CNN3DSeqReducedAttnPool CNN3DSeqMeanStdPool CNN3DSeqAttnPool CNN3DSeqReducedLSTM CNN3DSeqLSTM CNN2DSeqReducedMeanStdPool CNN2DSeqReducedAttnPool CNN2DSeqMeanStdPool CNN2DSeqAttnPool CNN2DSeqReducedLSTM CNN2DSeqLSTM; do
     for exp in data/processed/khan2021_*; do
         python3 covidecg/train.py --model $model $exp
     done
