463336c2bbf69bd072c1efa48e928a77c8a39135
scheuererra68323 <scheuererra68323@th-nuernberg.de>
2022-12-06 22:47:22 +0100

    update scripts for generating tasks

--- scripts/khan-10-generate-tasks.sh
+++ scripts/khan-10-generate-tasks.sh
@@ -1,19 +1,94 @@
+#!/usr/bin/env bash
 
 
-# khan2021 abnormal vs. normal
-rm -rf data/interim/khan_tasks_img/khan_abnormal_vs_normal/*
-mkdir data/interim/khan_tasks_img/khan_abnormal_vs_normal/1_abnormal
-mkdir data/interim/khan_tasks_img/khan_abnormal_vs_normal/0_normal
-ln -s `pwd`/data/interim/_khan2021_abnormal/*.png `pwd`/data/interim/khan_tasks_img/khan_abnormal_vs_normal/1_abnormal/
-ln -s `pwd`/data/interim/_khan2021_normal/*.png `pwd`/data/interim/khan_tasks_img/khan_abnormal_vs_normal/0_normal/
+###
+### Create task datasets in data/interim via symlinks
+###
 
 
-# khan2021 covid vs. normal
-rm -rf data/interim/khan_tasks_img/khan_covid_vs_normal/*
-mkdir data/interim/khan_tasks_img/khan_covid_vs_normal/1_covid
-mkdir data/interim/khan_tasks_img/khan_covid_vs_normal/0_normal
-ln -s `pwd`/data/interim/khan_feats_img/covid/*.png `pwd`/data/interim/khan_tasks_img/khan_covid_vs_normal/1_covid/
-ln -s `pwd`/data/interim/khan_feats_img/normal/*.png `pwd`/data/interim/khan_tasks_img/khan_covid_vs_normal/0_normal/
+##### DEV tasks
+
+
+
+######################################
+########## KHAN_FEATS_IMG ############
+######################################
+
+expDir=khan_tasks_img/khan_abnormal_vs_normal548
+rm -rf data/interim/$expDir
+mkdir -p data/interim/$expDir/1_abnormal
+mkdir -p data/interim/$expDir/0_normal
+ln -s `pwd`/data/interim/khan_feats_img/abnormal/*.png `pwd`/data/interim/$expDir/1_abnormal/
+ln -s `pwd`/data/interim/khan_feats_img/normal548/*.png `pwd`/data/interim/$expDir/0_normal/
+
+expDir=khan_tasks_img/khan_covid_vs_normal250
+rm -rf data/interim/$expDir
+mkdir -p data/interim/$expDir/1_covid
+mkdir -p data/interim/$expDir/0_normal
+ln -s `pwd`/data/interim/khan_feats_img/covid/*.png `pwd`/data/interim/$expDir/1_covid/
+ln -s `pwd`/data/interim/khan_feats_img/normal250/*.png `pwd`/data/interim/$expDir/0_normal/
+
+
+
+
+######################################
+######## KHAN_FEATS_IMGBLUR ##########
+######################################
+
+expDir=khan_tasks_imgblur/khan_abnormal_vs_normal548
+rm -rf data/interim/$expDir
+mkdir -p data/interim/$expDir/1_abnormal
+mkdir -p data/interim/$expDir/0_normal
+ln -s `pwd`/data/interim/khan_feats_imgblur/abnormal/*.png `pwd`/data/interim/$expDir/1_abnormal/
+ln -s `pwd`/data/interim/khan_feats_imgblur/normal548/*.png `pwd`/data/interim/$expDir/0_normal/
+
+expDir=khan_tasks_imgblur/khan_covid_vs_normal250
+rm -rf data/interim/$expDir
+mkdir -p data/interim/$expDir/1_covid
+mkdir -p data/interim/$expDir/0_normal
+ln -s `pwd`/data/interim/khan_feats_imgblur/covid/*.png `pwd`/data/interim/$expDir/1_covid/
+ln -s `pwd`/data/interim/khan_feats_imgblur/normal250/*.png `pwd`/data/interim/$expDir/0_normal/
+
+
+
+
+######################################
+####### KHAN_FEATS_IMGSHARPEN ########
+######################################
+
+expDir=khan_tasks_imgsharpen/khan_abnormal_vs_normal548
+rm -rf data/interim/$expDir
+mkdir -p data/interim/$expDir/1_abnormal
+mkdir -p data/interim/$expDir/0_normal
+ln -s `pwd`/data/interim/khan_feats_imgsharpen/abnormal/*.png `pwd`/data/interim/$expDir/1_abnormal/
+ln -s `pwd`/data/interim/khan_feats_imgsharpen/normal548/*.png `pwd`/data/interim/$expDir/0_normal/
+
+expDir=khan_tasks_imgsharpen/khan_covid_vs_normal250
+rm -rf data/interim/$expDir
+mkdir -p data/interim/$expDir/1_covid
+mkdir -p data/interim/$expDir/0_normal
+ln -s `pwd`/data/interim/khan_feats_imgsharpen/covid/*.png `pwd`/data/interim/$expDir/1_covid/
+ln -s `pwd`/data/interim/khan_feats_imgsharpen/normal250/*.png `pwd`/data/interim/$expDir/0_normal/
+
+
+
+######################################
+####### KHAN_FEATS_IMGSUPERRES #######
+######################################
+
+expDir=khan_tasks_imgsuperres/khan_abnormal_vs_normal548
+rm -rf data/interim/$expDir
+mkdir -p data/interim/$expDir/1_abnormal
+mkdir -p data/interim/$expDir/0_normal
+ln -s `pwd`/data/interim/khan_feats_imgsuperres/abnormal/*.png `pwd`/data/interim/$expDir/1_abnormal/
+ln -s `pwd`/data/interim/khan_feats_imgsuperres/normal548/*.png `pwd`/data/interim/$expDir/0_normal/
+
+expDir=khan_tasks_imgsuperres/khan_covid_vs_normal250
+rm -rf data/interim/$expDir
+mkdir -p data/interim/$expDir/1_covid
+mkdir -p data/interim/$expDir/0_normal
+ln -s `pwd`/data/interim/khan_feats_imgsuperres/covid/*.png `pwd`/data/interim/$expDir/1_covid/
+ln -s `pwd`/data/interim/khan_feats_imgsuperres/normal250/*.png `pwd`/data/interim/$expDir/0_normal/
 
 
 
@@ -24,13 +99,29 @@ ln -s `pwd`/data/interim/khan_feats_img/normal/*.png `pwd`/data/interim/khan_tas
 ### Generate train/test sets for each task
 ###
 
-rm -rf data/processed/khan_tasks_img/khan_abnormal_vs_normal
-splitfolders --output data/processed/khan_tasks_img/khan_abnormal_vs_normal --ratio .8 .2 -- data/interim/khan_tasks_img/khan_abnormal_vs_normal
-mv data/processed/khan_tasks_img/khan_abnormal_vs_normal/val data/processed/khan_tasks_img/khan_abnormal_vs_normal/test
+for expDir in khan_tasks_img/khan_abnormal_vs_normal548 khan_tasks_img/khan_covid_vs_normal250; do
+    rm -rf data/processed/$expDir
+    splitfolders --output data/processed/$expDir --ratio .8 .2 -- data/interim/$expDir
+    mv data/processed/$expDir/val data/processed/$expDir/test
+done
 
 
+for expDir in khan_tasks_imgblur/khan_abnormal_vs_normal548 khan_tasks_imgblur/khan_covid_vs_normal250; do
+    rm -rf data/processed/$expDir
+    splitfolders --output data/processed/$expDir --ratio .8 .2 -- data/interim/$expDir
+    mv data/processed/$expDir/val data/processed/$expDir/test
+done
 
-rm -rf data/processed/khan_tasks_img/khan_covid_vs_normal
-splitfolders --output data/processed/khan_tasks_img/khan_covid_vs_normal --ratio .8 .2 -- data/interim/khan_tasks_img/khan_covid_vs_normal
-mv data/processed/khan_tasks_img/khan_covid_vs_normal/val data/processed/khan_tasks_img/khan_covid_vs_normal/test
 
+for expDir in khan_tasks_imgsharpen/khan_abnormal_vs_normal548 khan_tasks_imgsharpen/khan_covid_vs_normal250; do
+    rm -rf data/processed/$expDir
+    splitfolders --output data/processed/$expDir --ratio .8 .2 -- data/interim/$expDir
+    mv data/processed/$expDir/val data/processed/$expDir/test
+done
+
+
+for expDir in khan_tasks_imgsuperres/khan_abnormal_vs_normal548 khan_tasks_imgsuperres/khan_covid_vs_normal250; do
+    rm -rf data/processed/$expDir
+    splitfolders --output data/processed/$expDir --ratio .8 .2 -- data/interim/$expDir
+    mv data/processed/$expDir/val data/processed/$expDir/test
+done
--- scripts/mmc-10-generate-tasks.sh
+++ scripts/mmc-10-generate-tasks.sh
@@ -1,50 +1,113 @@
 #!/usr/bin/env bash
 
+
+
+
+# Create undersampled versions of ctrl data for balanced classes tasks
+
+
+
+
+
+
+
+
+
+
+
+
+
 ###
 ### Create task datasets in data/interim via symlinks
 ###
 
-# mmc_tasks_raw/mmc_covid_vs_ctrl
-rm -rf data/interim/mmc_tasks_raw/mmc_covid_vs_ctrl/*
-mkdir data/interim/mmc_tasks_raw/mmc_covid_vs_ctrl/1_covid
-mkdir data/interim/mmc_tasks_raw/mmc_covid_vs_ctrl/0_ctrl
-ln -s `pwd`/data/interim/mmc_feats_raw/covid/*.csv `pwd`/data/interim/mmc_tasks_raw/mmc_covid_vs_ctrl/1_covid/
-ln -s `pwd`/data/interim/mmc_feats_raw/ctrl/*.csv `pwd`/data/interim/mmc_tasks_raw/mmc_covid_vs_ctrl/0_ctrl/
 
 
 
-# mmc_tasks_img/mmc_covid_vs_ctrl
-rm -rf data/interim/mmc_tasks_img/mmc_covid_vs_ctrl/*
-mkdir data/interim/mmc_tasks_img/mmc_covid_vs_ctrl/1_covid
-mkdir data/interim/mmc_tasks_img/mmc_covid_vs_ctrl/0_ctrl
-ln -s `pwd`/data/interim/mmc_feats_img/covid/*.png `pwd`/data/interim/mmc_tasks_img/mmc_covid_vs_ctrl/1_covid/
-ln -s `pwd`/data/interim/mmc_feats_img/ctrl/*.png `pwd`/data/interim/mmc_tasks_img/mmc_covid_vs_ctrl/0_ctrl/
+##### DEV tasks
+
+
+# # mmc_tasks_raw/mmc_covid20_vs_ctrl20
+# expDir=mmc_tasks_raw/mmc_covid20_vs_ctrl20
+# rm -rf data/interim/$expDir
+# mkdir -p data/interim/$expDir/1_covid
+# mkdir -p data/interim/$expDir/0_ctrl
+# ln -s `pwd`/data/interim/mmc_feats_raw/covid20/*.csv `pwd`/data/interim/$expDir/1_covid/
+# ln -s `pwd`/data/interim/mmc_feats_raw/ctrl20/*.csv `pwd`/data/interim/$expDir/0_ctrl/
+
+# # mmc_tasks_img/mmc_covid20_vs_ctrl20
+# expDir=mmc_tasks_img/mmc_covid20_vs_ctrl20
+# rm -rf data/interim/$expDir
+# mkdir -p data/interim/$expDir/1_covid
+# mkdir -p data/interim/$expDir/0_ctrl
+# ln -s `pwd`/data/interim/mmc_feats_img/covid20/*.png `pwd`/data/interim/$expDir/1_covid/
+# ln -s `pwd`/data/interim/mmc_feats_img/ctrl20/*.png `pwd`/data/interim/$expDir/0_ctrl/
+
+# mmc_tasks_lfcc/mmc_covid20_vs_ctrl20
+expDir=mmc_tasks_lfcc/mmc_covid20_vs_ctrl20
+rm -rf ./data/interim/$expDir
+mkdir -p ./data/interim/$expDir/1_covid
+mkdir -p ./data/interim/$expDir/0_ctrl
+ln -s `pwd`/data/interim/mmc_feats_lfcc/covid20/*.npy `pwd`/data/interim/$expDir/1_covid/
+ln -s `pwd`/data/interim/mmc_feats_lfcc/ctrl20/*.npy `pwd`/data/interim/$expDir/0_ctrl/
 
 
 
-# mmc_tasks_lfcc/mmc_covid_vs_ctrl
-rm -rf data/interim/mmc_tasks_lfcc/mmc_covid_vs_ctrl/*
-mkdir data/interim/mmc_tasks_lfcc/mmc_covid_vs_ctrl/1_covid
-mkdir data/interim/mmc_tasks_lfcc/mmc_covid_vs_ctrl/0_ctrl
-ln -s `pwd`/data/interim/mmc_feats_lfcc/covid/*.npy `pwd`/data/interim/mmc_tasks_lfcc/mmc_covid_vs_ctrl/1_covid/
-ln -s `pwd`/data/interim/mmc_feats_lfcc/ctrl/*.npy `pwd`/data/interim/mmc_tasks_lfcc/mmc_covid_vs_ctrl/0_ctrl/
+###########
+
+
+
+# # mmc_tasks_raw/mmc_covid_vs_ctrl610
+# expDir=mmc_tasks_raw/mmc_covid_vs_ctrl610
+# rm -rf data/interim/$expDir
+# mkdir -p data/interim/$expDir/1_covid
+# mkdir -p data/interim/$expDir/0_ctrl
+# ln -s `pwd`/data/interim/mmc_feats_raw/covid/*.csv `pwd`/data/interim/$expDir/1_covid/
+# ln -s `pwd`/data/interim/mmc_feats_raw/ctrl610/*.csv `pwd`/data/interim/$expDir/0_ctrl/
+
+
+# # mmc_tasks_img/mmc_covid_vs_ctrl610
+# rm -rf data/interim/mmc_tasks_img/mmc_covid_vs_ctrl610/*
+# mkdir -p data/interim/mmc_tasks_img/mmc_covid_vs_ctrl610/1_covid
+# mkdir -p data/interim/mmc_tasks_img/mmc_covid_vs_ctrl610/0_ctrl
+# ln -s `pwd`/data/interim/mmc_feats_img/covid/*.png `pwd`/data/interim/mmc_tasks_img/mmc_covid_vs_ctrl610/1_covid/
+# ln -s `pwd`/data/interim/mmc_feats_img/ctrl610/*.png `pwd`/data/interim/mmc_tasks_img/mmc_covid_vs_ctrl610/0_ctrl/
+
+
+# mmc_tasks_lfcc/mmc_covid_vs_ctrl610
+rm -rf ./data/interim/mmc_tasks_lfcc/mmc_covid_vs_ctrl610/*
+mkdir -p ./data/interim/mmc_tasks_lfcc/mmc_covid_vs_ctrl610/1_covid
+mkdir -p ./data/interim/mmc_tasks_lfcc/mmc_covid_vs_ctrl610/0_ctrl
+ln -s `pwd`/data/interim/mmc_feats_lfcc/covid/*.npy `pwd`/data/interim/mmc_tasks_lfcc/mmc_covid_vs_ctrl610/1_covid/
+ln -s `pwd`/data/interim/mmc_feats_lfcc/ctrl610/*.npy `pwd`/data/interim/mmc_tasks_lfcc/mmc_covid_vs_ctrl610/0_ctrl/
+
 
 
 ###
 ### Generate train/test sets for each task
 ###
 
-# mmc_tasks_raw/mmc_covid_vs_ctrl
-rm -rf data/processed/mmc_tasks_raw/mmc_covid_vs_ctrl/*
-python3 covidecg/data/split_train_test.py --test-ratio 0.2  --file-ext csv data/interim/mmc_tasks_raw/mmc_covid_vs_ctrl data/processed/mmc_tasks_raw/mmc_covid_vs_ctrl
+
+# for expDir in mmc_tasks_raw/mmc_covid20_vs_ctrl20 mmc_tasks_raw/mmc_covid_vs_ctrl610; do
+
+#     rm -rf data/processed/$expDir && mkdir data/processed/$expDir
+#     python3 covidecg/data/split_train_test.py --test-ratio 0.2  --file-ext csv data/interim/$expDir data/processed/$expDir
+
+# done
 
 
-# mmc_tasks_img/mmc_covid_vs_ctrl
-rm -rf data/processed/mmc_tasks_img/mmc_covid_vs_ctrl/*
-python3 covidecg/data/split_train_test.py --test-ratio 0.2  --file-ext png data/interim/mmc_tasks_img/mmc_covid_vs_ctrl data/processed/mmc_tasks_img/mmc_covid_vs_ctrl
+# for expDir in mmc_tasks_img/mmc_covid20_vs_ctrl20 mmc_tasks_img/mmc_covid_vs_ctrl610; do
+
+#     rm -rf data/processed/$expDir && mkdir data/processed/$expDir
+#     python3 covidecg/data/split_train_test.py --test-ratio 0.2  --file-ext png data/interim/$expDir data/processed/$expDir
+
+# done
 
 
-# mmc_tasks_lfcc/mmc_covid_vs_ctrl
-rm -rf data/processed/mmc_tasks_lfcc/mmc_covid_vs_ctrl/*
-python3 covidecg/data/split_train_test.py --test-ratio 0.2  --file-ext npy data/interim/mmc_tasks_lfcc/mmc_covid_vs_ctrl data/processed/mmc_tasks_lfcc/mmc_covid_vs_ctrl
+for expDir in mmc_tasks_lfcc/mmc_covid20_vs_ctrl20 mmc_tasks_lfcc/mmc_covid_vs_ctrl610; do
+
+    rm -rf ./data/processed/$expDir && mkdir ./data/processed/$expDir
+    python3 covidecg/data/split_train_test.py --test-ratio 0.2  --file-ext npy data/interim/$expDir data/processed/$expDir
+
+done
 
